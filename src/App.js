6c62a61d19b7f9f191ee14bf44555898358d301a
CPiliafas <PJRc3160@gmail.com>
2024-02-21 13:38:07 -0500

    - Added a button that will serve no function in the future and is merely there for aesthetic purposes. - Added a button component. - Will fix divs, add more sections, and continue styling the rest of the page in the future.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import React from 'react';
 import Navbar from './Components/navbar.js';
 import Footer from './Components/footer.js';
 import coffee from './Assets/coffee.jpg';
+import BasicButtons from './Components/Button.js';
+
 
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
-      <img src={coffee} alt="Coffee" position="absolute;" display="flex;" flex-direction="column;" object-fit="cover;" align-items="center;" height="500px;" width="100%;"/>
-      <h1>We Serve Out Of This World Coffee, Tea, and Baked Sweets!</h1>;
+      <img src={coffee} alt="Coffee" position="absolute;" display="flex;" flex-direction="column;" object-fit="cover;" align-items="center;" height="300px;" width="100%;"/>
+      <h1>We Serve Out Of This World Coffee, Tea, and Baked Sweets!</h1>
+      <div class="float1">
+        <div>
+          <h2>In Business since 2011</h2>
+          <p>Starting small, our team has been dedicated to growing our business as we strive towards continued improvement.</p>
+          <p>Click the button below to enter your email for updates, offers, and digital coupons to save money on your next purchase!</p>
+          <BasicButtons />
+        </div>
+      </div>
       <div class="container">
-
         <div class="float">
           <h2>Our Mission</h2>
           <p>To serve the best brewed, authentic coffee and providing positive customer experiences.</p>
---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { MdEmail } from "react-icons/md";
+
+export default function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button startIcon={MdEmail} variant="contained" color="secondary" justify-content="center">Join Our Mailing List!</Button>
+    </Stack>
+  );
+}
